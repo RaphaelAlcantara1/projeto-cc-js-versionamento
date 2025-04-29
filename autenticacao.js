73eb688c6447fb22028ad3f2799908aad0f0b331
Raphael Emanuell Alcântara - 102410790 <102410790@ulife.com.br>
2025-04-29 20:23:44 -0300

    atualiza mensagens de autenticação

--- autenticacao.js
+++ autenticacao.js
@@ -1,3 +1,11 @@
+  GNU nano 8.3                    autenticacao.js
+function autenticarUsuario(email, senha) {
+ if (email === "aluno@exemplo.com" && senha === "senha123") {
+ return "Autenticação bem-sucedida";
+ }
+ return "Falha na autenticação";
+}
+module.exports = { autenticarUsuario };
 function autenticarUsuario(email, senha) {
  if (email === "aluno@exemplo.com" && senha === "senha123") {
  return "Autenticação bem-sucedida";
